710c2a355e173187ee76b2978370519e190d81b9
mickeysrishti <Mukeshpathak345@gmail.com>
2024-01-02 21:47:40 +0530

    Code Updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ToDoApp from './components/ToDoApp'
 
 function App() {
   return (
-    <div className='w-full min-h-screen bg-zinc-900 text-white p-8'>
+    <div className='w-full min-h-screen bg-zinc-900 text-white pt-56'>
       <ToDoApp />
     </div>
   )
